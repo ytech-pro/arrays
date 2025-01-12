8411905abe89ac5103f38fe15035e8ce4812ee06
Ying Her <yher.prime@gmail.com>
2025-01-12 00:34:08 -0600

    created an array of favorite foods and find number of items using lenght

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -10,7 +10,8 @@ console.log('Animals are: ', animalArray);
 // 1. TODO: Make an array `favoriteFoods` with some favorite foods.
 //    Be sure to console.log your array of foods to the console with
 //    a message, similar to the example above.
-
+let favoriteFoods = ['pad krapao', 'green curry', 'papaya salad'];
+console.log('Favorite Foods are: ', favoriteFoods);
 
 // Array.length: A property that tells you how many items are in a given array.
 console.log('--- 2. Length of an array ---');
@@ -22,7 +23,8 @@ console.log('Number of animals:', numberOfAnimals);
 // 2. TODO: Create a variable `numberOfFoods` and use the .length property
 //    to assign it the number value of how many items are inside `favoriteFoods`.
 //    Don't forget to console.log `numberOfFoods` to make sure your code worked!
-
+let numberOfFoods = favoriteFoods.length
+console.log('Number of Foods:', numberOfFoods);
 
 // Accessing array items
 console.log('--- 3. Accessing items in an array ---');
